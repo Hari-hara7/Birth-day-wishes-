baa29f28de522a3a4e75a27f2ee75c3eac346019
Your Name <your.email@example.com>
2024-08-26 19:16:53 +0530

    hari

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+function openForm() {
+    document.getElementById("popupForm").style.display = "flex";
+}
+
+function closeForm() {
+    document.getElementById("popupForm").style.display = "none";
+}
