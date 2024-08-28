b5bccb265ea6d81a91ed746ec75085c4f0b7506a
Your Name <your.email@example.com>
2024-08-28 21:44:33 +0530

    deploy

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ function openForm() {
 function closeForm() {
     document.getElementById("popupForm").style.display = "none";
 }
+
+
+
+function create(){
+    let a=document.getElementById("my_id");
+}
